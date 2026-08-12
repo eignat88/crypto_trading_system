28a1a526d46e27c4ee8083c6ffe574cc2d8ed3db
eignat88 <eignatchenko88@gmail.com>
2026-08-12 13:52:35 +0300

    add derived dataset model versions

---
+++ app/config/model_versions.py
@@ -0,0 +1,6 @@
+"""Technical model-version identifiers used for reproducible derived datasets."""
+
+LEGACY_MODEL_VERSION = "legacy_unversioned"
+INDICATOR_MODEL_VERSION = "indicators_v2_hourly_volatility"
+REGIME_MODEL_VERSION = "regime_v2_hourly_volatility"
+EXECUTION_MODEL_VERSION = "backtest_hardened_v1"
